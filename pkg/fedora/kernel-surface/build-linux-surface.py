10eb4b6b200cc6c52fb3b892ccc7d565e5f892fa
Anthony Lee <108008609+orthogonaleety@users.noreply.github.com>
2026-09-22 12:34:39 +0100

    Update PACKAGE_TAG to kernel-7.2.6-0

--- pkg/fedora/kernel-surface/build-linux-surface.py
+++ pkg/fedora/kernel-surface/build-linux-surface.py
@@ -19,7 +19,7 @@ PACKAGE_NAME = "fedsurface"
 ## Fedora tags: kernel-X.Y.Z
 ## Upstream tags: vX.Y.Z
 ##
-PACKAGE_TAG = "kernel-7.2.5-0"
+PACKAGE_TAG = "kernel-7.2.6-0"
 
 ##
 ## The release number of the modified kernel package.
